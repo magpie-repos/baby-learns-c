9db0c4b3ada6d1b0b1258a6bd2d4e777db65742a
magpie <magpie@nest.tree>
2024-12-04 03:09:26 -0600

    Finished exercies from Programming in C ch2

---
+++ prog_in_c/ch2_ex5.c
@@ -0,0 +1,13 @@
+/*This program is a corrected version of an example provided as an end of chapter
+ * exercise in the book: Programming in C. The program sums several values and
+ * prints the result to the terminal.
+ */
+#include <stdio.h>
+
+int main (void) {
+    int sum;
+    sum = 25 + 37 - 19;
+    printf("The answer is %i.\n", sum);
+
+    return 0;
+}
