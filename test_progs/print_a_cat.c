62d20ba8c00c8b825d7ab32d132bb9727f20fb77
magpie <magpie@nest.tree>
2024-12-06 05:06:57 -0600

    made cat

---
+++ test_progs/print_a_cat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void) {
+
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("                                                                   \n");
+    printf("               ____    \\                  /      ____              \n");
+    printf("              [    ]    \\                /      [    ]             \n");
+    printf("             [  ()  ]    \\              /      [  ()  ]            \n");
+    printf("              [____]      \\            /        [____]             \n");
+    printf("       ======_             \\          /              _======       \n");
+    printf("            *=======,__     ~*********~     __,=======*            \n");
+    printf("                    * ^//  /  __   __  \\  //^ *                    \n");
+    printf("            _n===========5{  <  | |  >  }3=========n_              \n");
+    printf("      //===;*`       __+V8\\   *+* *+*  /8V+__     `*;===\\          \n");
+    printf("                 _===/`     *~w_   _w~*     `\\===_                 \n");
+    printf("              ==5/              +Y+               \\5==             \n");
+    printf("                                `|`                                \n");
+    printf("                                ,^,                              \n");
+    printf("                              __/|\\__                              \n");
+    printf("                                                                   \n");
+
+    return 0;
+}
+
