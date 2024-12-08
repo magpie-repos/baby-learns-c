0dd95a4e0e83b30e0c40d33370d0d783781d0309
magpie <magpie@nest.tree>
2024-12-08 02:21:01 -0600

    Finished Karl. He is jank but I love him anyway

--- test_progs/print_a_cat.c
+++ test_progs/print_a_cat.c
@@ -2,28 +2,31 @@
 
 int main (void) {
 
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("                                                                   \n");
-    printf("               ____    \\                  /      ____              \n");
-    printf("              [    ]    \\                /      [    ]             \n");
-    printf("             [  ()  ]    \\              /      [  ()  ]            \n");
-    printf("              [____]      \\            /        [____]             \n");
+    printf("                 /\\                             /\\                 \n");
+    printf("                /  \\                           /  \\                \n");
+    printf("               /    \\                         /    \\               \n");
+    printf("              /      \\                       /      \\              \n");
+    printf("             /        \\---------------------/        \\             \n");
+    printf("            /                                          \\            \n");
+    printf("           /                                            \\           \n");
+    printf("          /    ____    \\                  /      ____   \\           \n");
+    printf("         |    [    ]    \\                /      [    ]  |          \n");
+    printf("         |   [  ()  ]    \\              /      [  ()  ] |          \n");
+    printf("         |    [____]      \\            /        [____]  |          \n");
     printf("       ======_             \\          /              _======       \n");
-    printf("            *=======,__     ~*********~     __,=======*            \n");
-    printf("                    * ^//  /  __   __  \\  //^ *                    \n");
-    printf("            _n===========5{  <  | |  >  }3=========n_              \n");
-    printf("      //===;*`       __+V8\\   *+* *+*  /8V+__     `*;===\\          \n");
-    printf("                 _===/`     *~w_   _w~*     `\\===_                 \n");
-    printf("              ==5/              +Y+               \\5==             \n");
-    printf("                                `|`                                \n");
-    printf("                                ,^,                              \n");
-    printf("                              __/|\\__                              \n");
-    printf("                                                                   \n");
+    printf("         |  *=======,__     ~*********~     __,=======*  |         \n");
+    printf("         |          * ^//  /  __   __  \\  //^ *          |          \n");
+    printf("         |  _n===========5{  <  | |  >  }3=========n_    |         \n");
+    printf("      //===;*`       __+V8\\   *+* *+*  /8V+__      `*;===\\          \n");
+    printf("         |       _===/`     *~w_   _w~*     `\\===_      |          \n");
+    printf("         *\\    ==5/              +Y+               \\ 5== /*          \n");
+    printf("           \\                     `|`                    /           \n");
+    printf("            \\                    ,^,                   /            \n");
+    printf("             \\                 __/|\\__               /              \n");
+    printf("              \\_____________________________________/              \n");
+    printf("\n           This is Karl.                                         \n");
+    printf("                       Karl has witnessed you.                     \n");
+    printf("                                         Feel seen by Karl.        \n");
 
     return 0;
 }
